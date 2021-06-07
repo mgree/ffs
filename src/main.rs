5274b73fd8a247b2b0cb1e21c0aac6066d2dd006
Michael Greenberg <michael.greenberg@pomona.edu>
2021-06-07 13:33:06 -0400

    parsing json with serde

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use clap::{App, Arg};
+use serde_json::Value;
+
 fn main() {
-    println!("Hello, world!");
+    let config = App::new("ffs")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about("file fileystem")
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Sets the input file (defaults to '-', meaning STDIN)")
+                .default_value("-")
+                .index(1),
+        )
+        .get_matches();
+
+    let input_source = config.value_of("INPUT").unwrap();
+
+    let reader: Box<dyn std::io::BufRead> = if input_source == "-" {
+        Box::new(std::io::BufReader::new(std::io::stdin()))
+    } else {
+        let file = std::fs::File::open(input_source)
+            .unwrap_or_else(|e| panic!("Unable to open {} for JSON input: {}", input_source, e));
+        Box::new(std::io::BufReader::new(file))
+    };
+
+    let json: Value = serde_json::from_reader(reader).expect("JSON");
+
+    println!("{:?}", json);
 }
