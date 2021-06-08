a8fd7298844817fa08f8b06ae9db73d1b6529af5
Michael Greenberg <michael.greenberg@pomona.edu>
2021-06-07 21:37:23 -0400

    basic ro filesystem, objects but no arrays yet

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
+use std::path::Path;
 use clap::{App, Arg};
+use fuser::FileAttr;
+use fuser::FileType;
+use fuser::Filesystem;
+use fuser::MountOption;
+use fuser::ReplyAttr;
+use fuser::ReplyData;
+use fuser::ReplyDirectory;
+use fuser::ReplyEntry;
+use fuser::Request;
 use serde_json::Value;
 use std::collections::HashMap;
+use std::ffi::OsStr;
+use std::time::Duration;
 
 fn main() {
     let config = App::new("ffs")
@@ -8,12 +20,30 @@ fn main() {
         .author(env!("CARGO_PKG_AUTHORS"))
         .about("file fileystem")
         .arg(
+            Arg::with_name("AUTOUNMOUNT")
+                .help("Automatically unmount the filesystem when the mounting process exits")
+                .long("--autounmount"),
+        )
+        .arg(
+            Arg::with_name("MOUNT")
+                .help("Sets the mountpoint")
+                .required(true)
+                .index(1),
+        )
+        .arg(
             Arg::with_name("INPUT")
                 .help("Sets the input file (defaults to '-', meaning STDIN)")
                 .default_value("-")
-                .index(1),
+                .index(2),
         )
         .get_matches();
+
+    let autounmount = config.is_present("AUTOUNMOUNT");
+
+    let mount_point = Path::new(config.value_of("MOUNT").expect("mount point"));
+    if !mount_point.exists() {
+        panic!("Mount point {} does not exist.", mount_point.display());
+    }
 
     let input_source = config.value_of("INPUT").expect("input source");
 
@@ -32,45 +62,253 @@ fn main() {
     let fs = FS::from(json);
 
     println!("{:?}", fs);
+
+    let mut options = vec![MountOption::RO, MountOption::FSName(input_source.into())];
+    if autounmount {
+        options.push(MountOption::AutoUnmount);
+    }
+    fuser::mount2(fs, mount_point, &options).unwrap();
 }
 
 #[derive(Debug)]
 struct FS {
-    inodes: Vec<Inode>,
+    inodes: Vec<Option<Inode>>,
     timestamp: std::time::SystemTime,
 }
+
+const TTL: Duration = Duration::from_secs(300);
 
 #[derive(Debug)]
 struct Inode {
     parent: u64,
+    inum: u64,
     entry: Entry,
-}
-
-impl Inode {
-    pub fn error() -> Self {
-        Inode {
-            parent: 0,
-            entry: Entry::Error("invalid".into()),
-        }
-    }
 }
 
 #[derive(Debug)]
 enum Entry {
     File(String),
-    NamedDirectory(HashMap<String, u64>),
-    ListDirectory(Vec<u64>),
-    Error(String),
+    NamedDirectory(HashMap<String, DirEntry>),
+    ListDirectory(Vec<DirEntry>),
+}
+
+#[derive(Debug)]
+struct DirEntry {
+    kind: FileType,
+    inum: u64,
+}
+
+#[derive(Debug)]
+enum FSError {
+    NoSuchInode(u64),
+    InvalidInode(u64),
+}
+
+impl FS {
+    fn get(&self, inum: u64) -> Result<&Inode, FSError> {
+        let idx = inum as usize;
+
+        if idx >= self.inodes.len() {
+            return Err(FSError::NoSuchInode(inum));
+        }
+
+        match &self.inodes[idx] {
+            None => Err(FSError::InvalidInode(inum)),
+            Some(inode) => Ok(inode),
+        }
+    }
+
+    pub fn attr(&self, inode: &Inode) -> FileAttr {
+        let size = inode.entry.size();
+        let kind = inode.entry.kind();
+
+        let perm = if kind == FileType::Directory {
+            0o755
+        } else {
+            0o644
+        };
+
+        FileAttr {
+            ino: inode.inum,
+            atime: self.timestamp,
+            crtime: self.timestamp,
+            ctime: self.timestamp,
+            mtime: self.timestamp,
+            nlink: 1,
+            size,
+            blksize: 1,
+            blocks: size,
+            kind,
+            // TODO 2021-07-07 getpwnam upfront, store in fs
+            uid: 501, // first user on macOS
+            gid: 20,  // staff on macOS
+            perm,
+            rdev: 0,
+            padding: 0,
+            flags: 0, // weird macOS thing
+        }
+    }
+}
+
+impl Entry {
+    pub fn size(&self) -> u64 {
+        match self {
+            Entry::File(s) => s.len() as u64,
+            Entry::NamedDirectory(files) => {
+                files.iter().map(|(name, _inum)| name.len() as u64).sum()
+            }
+            Entry::ListDirectory(elts) => elts.len() as u64,
+        }
+    }
+
+    pub fn kind(&self) -> FileType {
+        match self {
+            Entry::File(_) => FileType::RegularFile,
+            Entry::NamedDirectory(_) | Entry::ListDirectory(_) => FileType::Directory,
+        }
+    }
+}
+
+impl Filesystem for FS {
+    fn lookup(&mut self, _req: &Request, parent: u64, name: &OsStr, reply: ReplyEntry) {
+        let dir = match self.get(parent) {
+            Err(_e) => {
+                reply.error(libc::ENOENT);
+                return;
+            }
+            Ok(inode) => inode,
+        };
+
+        let filename = match name.to_str() {
+            None => {
+                reply.error(libc::ENOENT);
+                return;
+            }
+            Some(name) => name,
+        };
+
+        match &dir.entry {
+            Entry::NamedDirectory(files) => match files.get(filename) {
+                None => {
+                    reply.error(libc::ENOENT);
+                    return;
+                }
+                Some(DirEntry { inum, .. }) => {
+                    let file = match self.get(*inum) {
+                        Err(_e) => {
+                            reply.error(libc::ENOENT);
+                            return;
+                        }
+                        Ok(inode) => inode,
+                    };
+
+                    reply.entry(&TTL, &self.attr(file), 0);
+                    return;
+                }
+            },
+            Entry::ListDirectory(_elts) => unimplemented!("TODO 2021-07-07 list directories"),
+            _ => {
+                reply.error(libc::ENOTDIR);
+                return;
+            }
+        }
+    }
+
+    fn getattr(&mut self, _req: &Request, ino: u64, reply: ReplyAttr) {
+        let file = match self.get(ino) {
+            Err(_e) => {
+                reply.error(libc::ENOENT);
+                return;
+            }
+            Ok(inode) => inode,
+        };
+
+        reply.attr(&TTL, &self.attr(file));
+    }
+
+    fn read(
+        &mut self,
+        _req: &Request,
+        ino: u64,
+        _fh: u64,
+        offset: i64,
+        _size: u32,
+        _flags: i32,
+        _lock: Option<u64>,
+        reply: ReplyData,
+    ) {
+        let file = match self.get(ino) {
+            Err(_e) => {
+                reply.error(libc::ENOENT);
+                return;
+            }
+            Ok(inode) => inode,
+        };
+
+        match &file.entry {
+            Entry::File(s) => reply.data(&s.as_bytes()[offset as usize..]),
+            _ => reply.error(libc::ENOENT),
+        }
+    }
+
+    fn readdir(
+        &mut self,
+        _req: &Request,
+        ino: u64,
+        _fh: u64,
+        offset: i64,
+        mut reply: ReplyDirectory,
+    ) {
+        let inode = match self.get(ino) {
+            Err(_e) => {
+                reply.error(libc::ENOENT);
+                return;
+            }
+            Ok(inode) => inode,
+        };
+
+        match &inode.entry {
+            Entry::File(_) => reply.error(libc::ENOTDIR),
+            Entry::ListDirectory(_elts) => unimplemented!("TODO 2021-06-07 list directory readdir"),
+            Entry::NamedDirectory(files) => {
+                let dot_entries = vec![
+                    (ino, FileType::Directory, "."),
+                    (inode.parent, FileType::Directory, ".."),
+                ];
+
+                let entries = files
+                    .into_iter()
+                    .map(|(filename, DirEntry { inum, kind })| (*inum, *kind, filename.as_str()));
+
+                for (i, entry) in dot_entries
+                    .into_iter()
+                    .chain(entries)
+                    .into_iter()
+                    .enumerate()
+                    .skip(offset as usize)
+                {
+                    if reply.add(entry.0, (i + 1) as i64, entry.1, entry.2) {
+                        break;
+                    }
+                }
+                reply.ok()
+            }
+        }
+    }
+}
+
+fn kind(v: &Value) -> FileType {
+    match v {
+        Value::Object(_) | Value::Array(_) => FileType::Directory,
+        _ => FileType::RegularFile,
+    }
 }
 
 impl From<Value> for FS {
     fn from(v: Value) -> Self {
-        let mut inodes: Vec<Inode> = Vec::new();
+        let mut inodes: Vec<Option<Inode>> = Vec::new();
         // get zero-indexing for free, with a nice non-zero check to boot
-        inodes.push(Inode {
-            parent: 0,
-            entry: Entry::Error("inode 0 is invalid".into()),
-        });
+        inodes.push(None);
         // TODO 2021-06-07 reserve based on guess or calculated size
 
         let mut next_id = fuser::FUSE_ROOT_ID;
@@ -99,8 +337,11 @@ impl From<Value> for FS {
                     children.reserve(vs.len());
 
                     for child in vs.into_iter() {
+                        children.push(DirEntry {
+                            inum: next_id,
+                            kind: kind(&child),
+                        });
                         worklist.push((inum, next_id, child));
-                        children.push(next_id);
                         next_id += 1;
                     }
 
@@ -111,8 +352,15 @@ impl From<Value> for FS {
                     children.reserve(fvs.len());
 
                     for (field, child) in fvs.into_iter() {
+                        children.insert(
+                            field,
+                            DirEntry {
+                                inum: next_id,
+                                kind: kind(&child),
+                            },
+                        );
                         worklist.push((inum, next_id, child));
-                        children.insert(field, next_id);
+                        // TODO 2021-06-07 check field name validity!
                         next_id += 1;
                     }
 
@@ -122,9 +370,13 @@ impl From<Value> for FS {
 
             let idx = inum as usize;
             if idx >= inodes.len() {
-                inodes.resize_with(idx + 1, || Inode::error());
+                inodes.resize_with(idx + 1, || None);
             }
-            inodes[idx] = Inode { parent, entry };
+            inodes[idx] = Some(Inode {
+                parent,
+                inum,
+                entry,
+            });
         }
         assert_eq!(inodes.len() as u64, next_id);
 
