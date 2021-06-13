ef4c0029656649ec7a7285a3e07db5c80ae50028
Michael Greenberg <michael.greenberg@pomona.edu>
2021-06-13 15:48:09 -0400

    support linux

--- tests/nlink.sh
+++ tests/nlink.sh
@@ -11,6 +11,18 @@ fail() {
     exit 1
 }
 
+if [ "$RUNNER_OS" = "Linux" ]; then
+    num_links() {
+        stat --format %h "$@"
+    }
+elif [ "$RUNNER_OS" = "macOS" ]; then
+    num_links() {
+        stat -f %l "$@"
+    }
+else
+    fail os
+fi
+
 MNT=$(mktemp -d)
 
 ffs "$MNT" ../json/nlink.json &
@@ -22,10 +34,10 @@ case $(ls) in
     (*) fail ls;;
 esac
 [ -d . ] && [ -d child1 ] && [ -f child2 ] && [ -d child3 ] || fail filetypes
-[ $(stat -r .      | cut -d' ' -f 4) -eq 4 ] || fail root   # parent + self + child1 + child3
-[ $(stat -r child1 | cut -d' ' -f 4) -eq 2 ] || fail child1 # parent + self
-[ $(stat -r child2 | cut -d' ' -f 4) -eq 1 ] || fail child2 # parent
-[ $(stat -r child3 | cut -d' ' -f 4) -eq 2 ] || fail child3 # parent + self
+[ $(num_links      .) -eq 4 ] || fail root   # parent + self + child1 + child3
+[ $(num_links child1) -eq 2 ] || fail child1 # parent + self
+[ $(num_links child2) -eq 1 ] || fail child2 # parent
+[ $(num_links child3) -eq 2 ] || fail child3 # parent + self
 cd - >/dev/null 2>&1
 umount "$MNT" || fail unmount
 sleep 1
