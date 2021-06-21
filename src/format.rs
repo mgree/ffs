6b5be0e0d5999bf6efa5fcdf7e5e7e3f36d0d79a
Michael Greenberg <michael.greenberg@pomona.edu>
2021-06-20 21:37:56 -0400

    remove TODO that won't quite work

--- src/format.rs
+++ src/format.rs
@@ -129,10 +129,12 @@ enum Node<V> {
     String(String),
     Bytes(Vec<u8>),
 
-    /// TODO 2021-06-18 can we make these Iter, to avoid any intermediate allocation/structures?
     List(Vec<V>),
     /// We use a `Vec` rather than a `Map` or `HashMap` to ensure we preserve
-    /// whatever order.
+    /// whatever order during renaming.
+    ///
+    /// It's a little bit annoying that, e.g., serde_json and toml use different
+    /// maps internally. :(
     Map(Vec<(String, V)>),
 }
 
