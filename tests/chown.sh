fee92a13da8f1df02a4405a7df56305c2631f2ea
Michael Greenberg <michael.greenberg@pomona.edu>
2021-06-23 19:41:54 -0400

    debug chown test

--- tests/chown.sh
+++ tests/chown.sh
@@ -18,7 +18,9 @@ ERR=$(mktemp)
 ffs --no-output "$MNT" ../json/object.json &
 PID=$!
 sleep 2
-chown :wheel "$MNT"/name 2>$ERR >&2 && fail "chgrp1: $(cat $ERR)"
+groups
+cat /etc/group
+chown :nobody "$MNT"/name 2>$ERR >&2 && fail "chgrp1: $(cat $ERR)"
 [ -s "$ERR" ] || fail "chgrp1 error: $(cat $ERR)"
 chown :$(groups | cut -d' ' -f 1) "$MNT"/name 2>$ERR >&2 || fail "chgrp2: $(cat $ERR)"
 [ -s "$ERR" ] && fail "chgrp2 error: $(cat $ERR)"
