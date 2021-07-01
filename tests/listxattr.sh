0edfcf6681f3789dff9922c95a23bcd4d13f611c
Michael Greenberg <michael.greenberg@pomona.edu>
2021-07-01 15:34:33 -0400

    implementations for everything, test for listxattr

---
+++ tests/listxattr.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+fail() {
+    echo FAILED: $1
+    if [ "$MNT" ]
+    then
+        cd
+        umount "$MNT"
+        rmdir "$MNT"
+    fi
+    exit 1
+}
+
+if [ "$RUNNER_OS" = "Linux" ] || [ "$(uname)" = "Linux" ]; then
+    listattr() {
+        getfattr --match=- "$@"
+    }
+elif [ "$RUNNER_OS" = "macOS" ] || [ "$(uname)" = "Darwin" ]; then
+    listattr() {
+        xattr -l "$@"
+    }
+else
+    fail os
+fi
+
+listattr_ok() {
+    listattr $1 | grep "user.type"
+}
+
+MNT=$(mktemp -d)
+
+ffs -m "$MNT" ../json/object.json &
+PID=$!
+sleep 2
+case $(ls "$MNT") in
+    (eyes*fingernails*human*name) ;;
+    (*) fail ls;;
+esac
+
+listattr_ok "$MNT" || fail root
+listattr_ok "$MNT"/name || fail name
+listattr_ok "$MNT"/eyes || fail eyes
+listattr_ok "$MNT"/fingernails || fail fingernails
+listattr_ok "$MNT"/human || fail human
+
+umount "$MNT" || fail unmount
+sleep 1
+
+kill -0 $PID >/dev/null 2>&1 && fail process
+
+rmdir "$MNT" || fail mount
