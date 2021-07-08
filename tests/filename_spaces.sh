fed1a4e8c3361efc373358fc92cd73cec164cb64
Michael Greenberg <michael.greenberg@pomona.edu>
2021-07-08 11:26:08 -0400

    whoops, commit spaces test

--- tests/filename_spaces.sh
+++ tests/filename_spaces.sh
@@ -6,19 +6,22 @@ fail() {
     then
         umount "$MNT"
         rmdir "$MNT"
-        rm "$OUT"
+        rm "$OUT" "$EXP"
     fi
     exit 1
 }
 
 MNT=$(mktemp -d)
 OUT=$(mktemp)
+EXP=$(mktemp)
 
-ffs -m "$MNT" --target yaml -o $OUT --munge filter ../yaml/spaces.yaml &
+printf -- "---\nfield one: 1\nfield two: 2\nfield three: 3" >"$EXP"
+
+ffs -m "$MNT" --target yaml -o "$OUT" --munge filter ../yaml/spaces.yaml &
 PID=$!
 sleep 2
 case $(ls "$MNT") in
-    (field one*field two) ;;
+    (field\ one*field\ two) ;;
     (*) fail ls;;
 esac
 [ "$(cat $MNT/field\ one)" -eq 1 ] || fail one
@@ -29,7 +32,11 @@ umount "$MNT" || fail unmount
 sleep 1
 kill -0 $PID >/dev/null 2>&1 && fail process
 
-grep "field three: 3" $OUT || fail three
+grep "field three: 3" $OUT >/dev/null 2>&1 || fail three
+
+sort $OUT >$OUT.yaml
+sort $EXP >$EXP.yaml
+diff $OUT.yaml $EXP.yaml || fail diff
 
 rmdir "$MNT" || fail mount
-rm "$OUT"
+rm "$OUT" "$EXP"
