986839211780a9fcb751f6eacc8c10aba584fdcf
Michael Greenberg <michael.greenberg@pomona.edu>
2021-07-08 11:06:18 -0400

    test with spaces in filenames

---
+++ tests/filename_spaces.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+fail() {
+    echo FAILED: $1
+    if [ "$MNT" ]
+    then
+        umount "$MNT"
+        rmdir "$MNT"
+        rm "$OUT"
+    fi
+    exit 1
+}
+
+MNT=$(mktemp -d)
+OUT=$(mktemp)
+
+ffs -m "$MNT" --target yaml -o $OUT --munge filter ../yaml/spaces.yaml &
+PID=$!
+sleep 2
+case $(ls "$MNT") in
+    (field one*field two) ;;
+    (*) fail ls;;
+esac
+[ "$(cat $MNT/field\ one)" -eq 1 ] || fail one
+[ "$(cat $MNT/field\ two)" -eq 2 ] || fail two
+echo 3 >"$MNT"/field\ three
+
+umount "$MNT" || fail unmount
+sleep 1
+kill -0 $PID >/dev/null 2>&1 && fail process
+
+grep "field three: 3" $OUT || fail three
+
+rmdir "$MNT" || fail mount
+rm "$OUT"
