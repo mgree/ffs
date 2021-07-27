35fbaf7ff62d6fd4d14dceaecf0a2f89e031c265
Michael Greenberg <michael.greenberg@pomona.edu>
2021-07-27 19:51:21 -0400

    don't try to run on logs, lol

--- bench/analysis.R
+++ bench/analysis.R
@@ -1,10 +1,15 @@
 library(ggplot2)
 
 setwd("~/ffs/bench")
-sizes <- data.frame(read.csv("20210727_run.log"))
-names(sizes)
+sizes <- data.frame(read.csv("runs/20210727_run.log"))
 
-ggplot(sizes) + geom_point(aes(size,ns,colour=source), data=subset(sizes, activity %in% "reading json"))
-ggplot(sizes) + geom_point(aes(size,ns,colour=source), data=subset(sizes, activity %in% "inodes"))
-ggplot(sizes) + geom_point(aes(size,ns,colour=source), data=subset(sizes, activity %in% "saving"))
-ggplot(sizes) + geom_point(aes(size,ns,colour=source), data=subset(sizes, activity %in% "writing"))
+#ggplot(subset(sizes, activity %in% "reading json")) + geom_smooth(method="lm", aes(x=size, y=ns), formula=(y ~ x), se=FALSE, linetype = 1) + geom_point(aes(size,ns,colour=source))
+#ggplot(subset(sizes, activity %in% "inodes")) + geom_smooth(method="lm", aes(x=size, y=ns), formula=(y ~ x), se=FALSE, linetype = 1) + geom_point(aes(size,ns,colour=source))
+#ggplot(subset(sizes, activity %in% "saving")) + geom_smooth(method="lm", aes(x=size, y=ns), formula=(y ~ x), se=FALSE, linetype = 1) + geom_point(aes(size,ns,colour=source))
+#ggplot(subset(sizes, activity %in% "writing")) + geom_smooth(method="lm", aes(x=size, y=ns), formula=(y ~ x), se=FALSE, linetype = 1) + geom_point(aes(size,ns,colour=source))
+
+ggplot(sizes) +
+  xlab("File size (MiB)") + ylab("Time (ms, log10)") + scale_colour_discrete(name = "Activity") +
+  scale_x_continuous(limits=c(0,3100), breaks=c(0,1024,2048,3072), labels=c("0MiB","1MiB","2MiB","3MiB")) +
+  scale_y_continuous(breaks=c(0,2,4,6,8,10),labels=c("0ms", "0.01ms", "0.1ms", "1ms", "10ms", "100ms")) +
+  geom_point(aes(size/1024,log(ns,base=10),colour=activity))
