dea874e125814df96c3ce2cc7e1ccbde826b4463
Michael Greenberg <michael.greenberg@pomona.edu>
2021-07-29 13:16:16 -0400

    hopefully almost at a working pipeline

--- run_bench.sh
+++ run_bench.sh
@@ -8,7 +8,10 @@ BENCH="../${TIMESTAMP}_bench.log"
 ./bench.sh >"$BENCH"
 
 ./mk_micro.sh
-MICRO="../${TIMESTAMP}_micro.log"
+MICRO_RAW=$(mktemp)
 ./bench.sh -d micro >"$MICRO"
+
+MICRO="../${TIMESTAMP}_micro.log"
+./fixup_micro.sh >"$MICRO"
 
 ./generate_charts.R "$BENCH" "$MICRO"
