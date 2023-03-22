3d05436b98ff8e844cb45eb10f3b8e458db7c602
nad <58147198+nad2040@users.noreply.github.com>
2023-03-22 14:12:35 -0400

    not working pack.rs

--- src/bin/pack.rs
+++ src/bin/pack.rs
@@ -10,22 +10,68 @@ use std::str::FromStr;
 use ffs::config::Config;
 // use ffs::config::Output;
 use ffs::format;
-// use format::Format;
+use ffs::time_ns;
+use format::Format;
 use format::Nodelike;
 use format::Typ;
-// use format::json::Value as JsonValue;
-// use format::toml::Value as TomlValue;
+use format::json::Value as JsonValue;
+use format::toml::Value as TomlValue;
 use format::yaml::Value as YamlValue;
 
 use ::xattr;
+use ::walkdir;
+use walkdir::WalkDir;
 
 #[allow(dead_code)]
 #[allow(unused_variables)]
-fn pack<V>(root: V, root_path: PathBuf, config: &Config) -> std::io::Result<()>
+fn pack<V>(folder: PathBuf, config: &Config) -> std::io::Result<V>
 where
     V: Nodelike + std::fmt::Display + Default,
 {
-    Ok(())
+    // TODO (nad) fix this
+    // 1. change folder to path
+    // 2. get the xattr of path.
+    // 3. if it is of map or list, use walker of depth 1
+    // 4. if it is a file, use the xattr to determine the type and read the file.
+    // 5. after this function returns the highest level Value, use to_writer
+
+    // let walker = WalkDir::new(folder).max_depth(1).into_iter();
+    // for entry in walker {
+    //     let e = entry?;
+    //     let path = e.path();
+    //     println!("{:?}", &path.display());
+    //     let typ = xattr::get(path, "user.type")?.unwrap();
+    //     let typ = str::from_utf8(&typ).unwrap();
+    //     match Typ::from_str(typ) {
+    //         Ok(t) => {
+    //             let file = fs::File::open(&path).unwrap();
+    //             let mut reader = BufReader::new(&file);
+    //             let mut contents: Vec<u8> = Vec::new();
+    //             reader.read_to_end(&mut contents);
+    //             match String::from_utf8(contents.clone()) {
+    //                 Ok(mut contents) if t != Typ::Bytes => {
+    //                     if config.add_newlines && contents.ends_with('\n') {
+    //                         contents.truncate(contents.len() - 1);
+    //                     }
+    //                     // TODO 2021-06-24 trim?
+    //                     V::from_string(t, contents, &config)
+    //                 }
+    //                 Ok(_) | Err(_) => V::from_bytes(contents, &config),
+    //             }
+    //         }
+    //         Err(_) => {
+    //             match typ {
+    //                 "map" => {
+    //                     println!("map");
+    //                 }
+    //                 "list" => {
+    //                     println!("list");
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    Ok()
 }
 
 
@@ -44,18 +90,36 @@ fn main() -> std::io::Result<()> {
      */
 
     // test code that should be run after unpacking invoice.yaml in the cwd.
-    let file = fs::File::open("invoice/invoice").unwrap();
-    println!("file: {:?}", file);
-    let mut reader = BufReader::new(file);
-    let mut contents = String::new();
-    reader.read_to_string(&mut contents).unwrap();
-    println!("{}", contents);
-    let typ = xattr::get("invoice/invoice", "user.type")?.unwrap();
-    let typ = Typ::from_str(str::from_utf8(&typ).unwrap());
-    let typ = typ.unwrap();
-    println!("type: {:?}", typ);
+    let mount = match &config.mount {
+        Some(m) => m,
+        None => {
+            println!("No mount point given");
+            return Ok(());
+        }
+    };
 
-    let val = YamlValue::from_string(typ, contents, &config);
-    println!("val: {:?}", val);
+
+    let folder = PathBuf::from(mount);
+
+    let writer = match config.output_writer() {
+        Some(writer) => writer,
+        None => return Ok(()),
+    };
+
+    match &config.output_format {
+        Format::Json => {
+            pack(folder, &config);
+            // v.to_writer(writer, &config.pretty);
+        }
+        Format::Toml => {
+            pack(folder, &config);
+            // v.to_writer(writer, &config.pretty);
+        }
+        Format::Yaml => {
+            pack(folder, &config);
+            // v.to_writer(writer, &config.pretty);
+        }
+    }
+
     Ok(())
 }
